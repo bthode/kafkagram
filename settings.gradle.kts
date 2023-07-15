0e787abd498f1514851afef178cbc7456c48e544
Bryan Thode <bryan@tatq.com>
2023-07-15 08:23:16 -0500

    Bump versions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,8 +5,8 @@ pluginManagement {
 plugins {
     id("io.ktor.plugin") version "2.3.2" apply false
     id("org.jmailen.kotlinter") version "3.15.0" apply false
-    kotlin("jvm") version "1.8.22" apply false
-    kotlin("plugin.serialization") version "1.8.22" apply false
+    kotlin("jvm") version "1.9.0" apply false
+    kotlin("plugin.serialization") version "1.9.0" apply false
 }
 
 rootProject.name = "kafkagram"
